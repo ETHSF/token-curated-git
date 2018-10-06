b7831dfe7b78fb2b09cd4210bbb894b325c643d7
jvluso <jvluso@yahoo.com>
2018-10-06 13:59:36 -0700

    touch

---
+++ dist/merge.js
@@ -0,0 +1,31 @@
+import initAragonJS from './aragonjs-wrapper';
+initAragonJS(___dao___, ___ens___, {
+  accounts: web3.accounts,
+  provider: web3.currentProvider,
+  onApps: async apps => {
+    appsLoaded = true;
+    await tryFindTransactionPath();
+  }
+}).then(async initializedWrapper => {
+  wrapper = initializedWrapper;
+  await tryFindTransactionPath();
+}).catch(err => {
+  reporter.error('Error inspecting DAO');
+  reporter.debug(err);
+  process.exit(1);
+});
+
+const tryFindTransactionPath = async () => {
+  if (appsLoaded && wrapper) {
+    ctx.transactionPath = await wrapper.getTransactionPath(___destination___, "approve", [___githash___, "0x00"]);
+    let tx = ctx.transactionPath[0];
+
+    if (!tx) {
+      throw new Error('Cannot find transaction path for executing action');
+    }
+
+    const estimatedGas = await web3.eth.estimateGas(ctx.transactionPath[0]);
+    tx.gas = parseInt(GAS_ESTIMATE_FUZZ_FACTOR * estimatedGas);
+    await web3.eth.sendTransaction(ctx.transactionPath[0]);
+  }
+};
--- src/merge.js
+++ src/merge.js
@@ -1,24 +1,9 @@
 import initAragonJS from './aragonjs-wrapper'
 
 
-const tryFindTransactionPath = async () => {
-  if (appsLoaded && wrapper) {
-    ctx.transactionPath = await wrapper.getTransactionPath(***destination***,approve,[***githash***,"0x00"])
 
-    let tx = ctx.transactionPath[0]
-
-    if (!tx) {
-      throw new Error('Cannot find transaction path for executing action')
-    }
-
-    const estimatedGas = await web3.eth.estimateGas(ctx.transactionPath[0])
-    tx.gas = parseInt(GAS_ESTIMATE_FUZZ_FACTOR * estimatedGas)
-    await web3.eth.sendTransaction(ctx.transactionPath[0])
-  }
-}
-
-initAragonJS(dao, apm['ens-registry'], {
-  accounts: ctx.accounts,
+initAragonJS(___dao___, ___ens___, {
+  accounts: web3.accounts,
   provider: web3.currentProvider,
   onApps: async apps => {
     appsLoaded = true
@@ -34,3 +19,20 @@ initAragonJS(dao, apm['ens-registry'], {
   reporter.debug(err)
   process.exit(1)
 })
+
+
+const tryFindTransactionPath = async () => {
+  if (appsLoaded && wrapper) {
+    ctx.transactionPath = await wrapper.getTransactionPath(___destination___,"approve",[___githash___,"0x00"])
+
+    let tx = ctx.transactionPath[0]
+
+    if (!tx) {
+      throw new Error('Cannot find transaction path for executing action')
+    }
+
+    const estimatedGas = await web3.eth.estimateGas(ctx.transactionPath[0])
+    tx.gas = parseInt(GAS_ESTIMATE_FUZZ_FACTOR * estimatedGas)
+    await web3.eth.sendTransaction(ctx.transactionPath[0])
+  }
+}
