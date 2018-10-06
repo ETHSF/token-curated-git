428cd558e6f2d741bfcf19054a62b3eebcf0b50a
William Griffiths <willjgriff@gmail.com>
2018-10-06 12:53:44 -0700

    Added compiled ABI contract. Configured to use older solc compiler version to enable compiling.

--- app/truffle.js
+++ app/truffle.js
@@ -5,5 +5,11 @@ module.exports = {
       port: 8545,
       network_id: '*'
     }
-  }
+  },
+    compilers: {
+        solc: {
+            version: "0.4.24",  // ex:  "0.4.20". (Default: Truffle's installed solc)
+        }
+    }
+
 }
