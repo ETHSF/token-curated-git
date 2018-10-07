e81654e06246e70d3800ace23e1c26f597ce12c6
William Griffiths <willjgriff@gmail.com>
2018-10-06 18:09:59 -0700

    Added fix to enable CLI for Validate.js.

--- src/validate.js
+++ src/validate.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 const tokenCuratedGitContract = require("../app/build/contracts/TokenCuratedGit.json")
-// const tcgConfig = require("../../tcg-config.json")
+const tcgConfig = require("../tcg-config.json")
 
 const Web3 = require("web3")
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'))
 
-const tokenCuratedGit = new web3.eth.Contract(tokenCuratedGitContract.abi, "0x4E4a2B89Ea27338D323C0dCA22b1ffbA62db1177")
+const tokenCuratedGit = new web3.eth.Contract(tokenCuratedGitContract.abi, tcgConfig.tokenCuratedGitAddress)
 
 let commitHash = require('child_process')
     .execSync('git rev-parse HEAD')
